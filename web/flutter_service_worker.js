a0134c1d12264f47fa9646b38b90f9ce5f0c2893
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-12-31 13:04:44 +0700

    fix: Rebuild things for some magic reasons.

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -35,8 +35,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1007955c6a337b51d2f90efe87d52267",
-"/": "1007955c6a337b51d2f90efe87d52267",
+"index.html": "8099f07dffcf60e505116617ab668dba",
+"/": "8099f07dffcf60e505116617ab668dba",
 "main.dart.js": "697b14af483169b35dae1856708e60a2",
 "manifest.json": "0555ecb1622c893b8d8ee496baaf9af3",
 "version.json": "d87f3d636046bacfbaa1496b2d19371e"
